a84151a500941c762cf61a9561c61e4c10e1454c
Catnip9374 <100660925+Catnip9374@users.noreply.github.com>
2022-04-26 22:57:04 +0930

    Add files via upload

--- MapSquare.cpp
+++ MapSquare.cpp
@@ -3,7 +3,7 @@
 
 std::vector<int> MapSquare::map(std::vector<int> a){
     std::vector<int> ans;
-    for (int i = 0; i < a.size();++i){
+    for (size_t i = 0; i < a.size();++i){
         ans.push_back(f(a[i]));
     }
     return ans;
